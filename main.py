4bb7e38f8fed21d1c1e760faca0d200e43813d75
gvrsrg <gvrsrg@gmail.com>
2023-02-20 00:23:32 +0200

    sending random motivation

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import smtplib
 import credentials
+import datetime as dt
+import random
 
-my_email = credentials.email
-password = credentials.password
-to_address = credentials.to_address
+def send_mail(text):
 
-connection = smtplib.SMTP(credentials.smtp_address)
-connection.starttls()
-connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addrs=to_address, msg="Hello")
-connection.close()
+    my_email = credentials.email
+    password = credentials.app_password
+    to_address = credentials.to_address
+    with smtplib.SMTP(credentials.smtp_address) as conn:
+        conn.starttls()
+        conn.login(user=my_email, password=password)
+        conn.sendmail(from_addr=my_email, to_addrs=to_address, msg=f"Subject:Hello\n\n{text}")
+
+now = dt.datetime.now()
+weekday = now.weekday()
+
+if weekday==0:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        line = random.choice(all_quotes)
+        send_mail(line)
+
