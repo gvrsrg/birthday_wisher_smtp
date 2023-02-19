a50c072fcb61b2bcd9f8b5abd4eefaa91d4db395
gvrsrg <gvrsrg@gmail.com>
2023-02-19 23:57:52 +0200

    1

--- main.py
+++ main.py
@@ -2,5 +2,11 @@ import smtplib
 import credentials
 
 my_email = credentials.email
+password = credentials.password
+to_address = credentials.to_address
 
 connection = smtplib.SMTP(credentials.smtp_address)
+connection.starttls()
+connection.login(user=my_email, password=password)
+connection.sendmail(from_addr=my_email, to_addrs=to_address, msg="Hello")
+connection.close()
